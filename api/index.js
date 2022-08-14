95371cdc73a28b62fbdeec382d9afef2f2401888
vvtihomirov <vtihomirov@lintech.ru>
2022-08-14 18:31:52 +0300

    commit

--- api/index.js
+++ api/index.js
@@ -1,53 +1,26 @@
-// const aneks = require('../data/result.json')
+const aneks = require('../data/result.json')
 
-// const https = require('https')
+const http = require('http');
 
-// const fs = require('fs');
+const hostname = 'localhost';
+const port = 3030;
 
-// const options = {
-
-//     key: fs.readFileSync('key.pem'),
-//     cert: fs.readFileSync('cert.pem')
-// };
-
-// const app = function (req, res) {
-//     console.log(req)
-//     res.writeHead(200);
-//     res.end("hello world\n");
-// }
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    // aneks['messages'][Math.round(Math.random() * 100)]['text']
 
 
-// https.createServer(options, app).listen(3000);
+    res.write(JSON.stringify({
+        "response": {
 
-// Для асинхронной работы используется пакет micro.
-// const { json } = require('micro');
+            "text": aneks['messages'][Math.round(Math.random() * 100)]['text'],
+            "tts": aneks['messages'][Math.round(Math.random() * 100)]['text']
+        },
+        "version": "1.0"
+    }))
+    res.end()
+});
 
-// Запуск асинхронного сервиса.
-export default function handler(req, res) {
-
-    // Из запроса извлекаются свойства request, session и version.
-    // const { request, session, version } = await json(req);
-
-    res.status(200).send(`Hello!`);
-
-    // console.log(request.original_utterance);
-
-    // // В тело ответа вставляются свойства version и session из запроса.
-    // // Подробнее о формате запроса и ответа — в разделе Протокол работы навыка.
-    // res.end(JSON.stringify(
-    //     {
-    //         version,
-    //         session,
-    //         response: {
-    //             // В свойстве response.text возвращается исходная реплика пользователя.
-    //             // Если навык был активирован без дополнительной команды,
-    //             // пользователю нужно сказать "Hello!".
-    //             text: request.original_utterance || 'Hello!',
-
-    //             // Свойство response.end_session возвращается со значением false,
-    //             // чтобы диалог не завершался.
-    //             end_session: false,
-    //         },
-    //     }
-    // ));
-}
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
