3eff57ba4878dee7ade42fe468f66c62843c59f5
vvtihomirov <vtihomirov@lintech.ru>
2022-08-14 16:13:31 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
-const aneks = require('../data/result.json')
+// const aneks = require('../data/result.json')
 
-const https = require('https')
+// const https = require('https')
 
-const fs = require('fs');
+// const fs = require('fs');
 
-const options = {
+// const options = {
 
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('cert.pem')
+// };
+
+// const app = function (req, res) {
+//     console.log(req)
+//     res.writeHead(200);
+//     res.end("hello world\n");
+// }
+
+
+// https.createServer(options, app).listen(3000);
+
+// Для асинхронной работы используется пакет micro.
+const { json } = require('micro');
+
+// Запуск асинхронного сервиса.
+module.exports = async (req, res) => {
+
+    // Из запроса извлекаются свойства request, session и version.
+    const { request, session, version } = await json(req);
+
+    // В тело ответа вставляются свойства version и session из запроса.
+    // Подробнее о формате запроса и ответа — в разделе Протокол работы навыка.
+    res.end(JSON.stringify(
+        {
+            version,
+            session,
+            response: {
+                // В свойстве response.text возвращается исходная реплика пользователя.
+                // Если навык был активирован без дополнительной команды,
+                // пользователю нужно сказать "Hello!".
+                text: request.original_utterance || 'Hello!',
+
+                // Свойство response.end_session возвращается со значением false,
+                // чтобы диалог не завершался.
+                end_session: false,
+            },
+        }
+    ));
 };
-
-const app = function (req, res) {
-    console.log(req)
-    res.writeHead(200);
-    res.end("hello world\n");
-}
-
-
-https.createServer(options, app).listen(3000);
